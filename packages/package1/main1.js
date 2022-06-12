96e027fc086633ec01e2f9aa66a25a18bc936f5c
nnhjs <nnhungjs@gmail.com>
2022-06-12 20:58:18 +0700

    chore: update packages and config eslint

---
+++ packages/package1/main1.js
@@ -0,0 +1,7 @@
+console.log('package1/main1js')
+export const printf = (string) => {
+  console.log(string)
+  return string
+}
+
+export default printf
---
+++ packages/package2/main2.js
@@ -0,0 +1,4 @@
+import sum from "./sum"
+
+sum(1, 2)
+console.log("package2/main2.js")
